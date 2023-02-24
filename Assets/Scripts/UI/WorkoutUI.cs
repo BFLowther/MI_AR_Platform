e21d66aa90da48e6e750a31788253cfa0d8dbb64
Melisa Rogers <maj4683@gmail.com>
2023-02-23 20:49:37 -0600

    Adds functionality to more info page

--- Assets/Scripts/UI/WorkoutUI.cs
+++ Assets/Scripts/UI/WorkoutUI.cs
@@ -2,23 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class WorkoutUI : MonoBehaviour
 {
     public List<ExerciseItem> exerciseItems = new List<ExerciseItem>();
 
+    [Header("More Info Page")]
+    public GameObject moreInfoPageGO;
+    public TMP_Text titleText;
+    public TMP_Text infoText;
+    private ExerciseItem curItem;
+    
+
     private void Start()
     {
         if (UnlockManager.Instance.todaysUnlocks.Count > 0)
         {
-            SetExerciseItems();
+            SetUp();
         }
+
+
     }
 
-    private void SetExerciseItems()
+    private void SetUp()
     {
         for(int i=0; i<exerciseItems.Count; i++)
         {
+            if(UnlockManager.Instance.tryingToUnlock == exerciseItems[i].ExerciseName)
+            {
+                MoreInfoPage(exerciseItems[i].ExerciseName, exerciseItems[i].infoString, exerciseItems[i]);
+            }
+
             if (UnlockManager.Instance.todaysUnlocks.Contains(exerciseItems[i].ExerciseName))
             {
                 exerciseItems[i].SetToggle();
@@ -29,5 +44,20 @@ public class WorkoutUI : MonoBehaviour
     public void QRScanner()
     {
         SceneManager.LoadScene("QRCodeTest");
+    }
+
+    public void MoreInfoPage(string title, string info, ExerciseItem item)
+    {
+
+        titleText.text = title;
+        infoText.text = info;
+        curItem = item;
+        moreInfoPageGO.SetActive(true);
+    }
+
+    public void Complete()
+    {
+        UnlockManager.Instance.ConfirmUnlock();
+        curItem.SetToggle();
     }
 }
