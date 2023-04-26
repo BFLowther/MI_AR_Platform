e67268722984bcb5b64782972390be2ce9a2b0a2
EricWRogers <ericwilliamrogers@gmail.com>
2023-04-26 15:58:37 -0500

    Working map

--- Assets/Scripts/MoveGPSPlayer.cs
+++ Assets/Scripts/MoveGPSPlayer.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class MoveGPSPlayer : MonoBehaviour
 {
+    public Vector3 targetLocalPosition;
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        if(Vector2.Distance(transform.localPosition, targetLocalPosition) < ((speed * 1.5f) * Time.deltaTime)) {
+            transform.localPosition = targetLocalPosition;
+        } else {
+            Vector2 direction = targetLocalPosition - transform.localPosition;
+            transform.localPosition = ((Vector2)transform.localPosition) + (direction.normalized * speed * Time.deltaTime);
+        }
+    }
     public void Move(Vector2 _position)
     {
-        transform.position = new Vector3(_position.x, _position.y, 0.0f);
+        Debug.Log(transform.localPosition);
+        targetLocalPosition = new Vector3(_position.x, _position.y, 0.0f);
+        Debug.Log(transform.localPosition);
     }
 }
