7efb99f79786d23fb296b2ba1995a2213b62829e
Melisa Rogers <maj4683@gmail.com>
2023-04-26 18:59:23 -0700

    Adds UI to Login Scene

---
+++ Assets/Scripts/UI/LoginUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LoginUI : MonoBehaviour
+{
+    public TMP_InputField usernameInput;
+    public TMP_InputField passwordInput;
+
+    public LoginHelper loginHelper;
+
+    public void SignUp()
+    {
+        loginHelper.SignUp(usernameInput.text, passwordInput.text);
+    }
+    public void SignIn()
+    {
+        loginHelper.SignIn(usernameInput.text, passwordInput.text);
+    }
+
+    public void LoadMapScene()
+    {
+        SceneManager.LoadScene("Map");
+    }
+}
